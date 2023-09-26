5e37eb67c53e8567deb19c9fc92ec6acac84e1bf
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-26 13:06:19 -0400

    Added images to the project listing

--- client/src/components/ProjectListing.js
+++ client/src/components/ProjectListing.js
@@ -4,9 +4,10 @@ import './styles/ProjectListing.css';
 function ProjectListing() {
   // Static data for now. This will be fetched from the backend later.
   const projects = [
-    { id: 1, title: "Build a Website" },
-    { id: 2, title: "Design a Logo" }
+    { id: 1, title: "Build a Website", imageUrl: "https://entrepreneurhandbook.co.uk/wp-content/uploads/2019/10/Website-builder.jpg.webp" },
+    { id: 2, title: "Design a Logo", imageUrl: "https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-04-2.-a-by-nick-matey.jpg" }
   ];
+  
 
   return (
     <div className="project-listing-container">
@@ -14,12 +15,14 @@ function ProjectListing() {
       <div className="projects-grid">
         {projects.map(project => (
           <div key={project.id} className="project-card">
+            <img src={project.imageUrl} alt={project.title} className="project-image" />
             <a href={`/projects/${project.id}`}>{project.title}</a>
           </div>
         ))}
       </div>
     </div>
   );
+  
 }
 
 export default ProjectListing;
