b75c3264e60a6bc0fbec683369ac268d92e80875
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-26 13:05:37 -0400

    Removed unnecessary imports

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faHome, faProjectDiagram, faRegistered, faUserGear } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faProjectDiagram, faUserGear } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
