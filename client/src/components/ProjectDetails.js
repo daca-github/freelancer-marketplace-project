163dbe20d9cea0cff805486e91ec625d45a18509
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-26 12:04:00 -0400

    Setting up ProjectDetails.js with static data

---
+++ client/src/components/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ProjectDetails() {
+  // Static data for now. This will be fetched based on project ID later.
+  const project = {
+    title: "Build a Website",
+    description: "Need a responsive website for my cafe.",
+    budget: "$500"
+  };
+
+  return (
+    <div>
+      <h2>{project.title}</h2>
+      <p>{project.description}</p>
+      <p>Budget: {project.budget}</p>
+    </div>
+  );
+}
+
+export default ProjectDetails;
