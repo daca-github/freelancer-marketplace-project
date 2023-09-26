56cf0fb6ef3b5f66d88bdc61aa4e6d008f1cad09
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-26 12:52:42 -0400

    Added icons to the Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faHome, faProjectDiagram, faRegistered, faUserGear } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      <Link to="/"><li>Home <FontAwesomeIcon icon={faHome} /></li></Link>
+      <Link to="/projects"><li>Projects <FontAwesomeIcon icon={faProjectDiagram} /></li></Link>
+      <Link to="/login"><li>Login <FontAwesomeIcon icon={faUserGear} /></li></Link>
+      
     </nav>
   );
 }
