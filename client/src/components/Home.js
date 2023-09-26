de0b9af76549a3abffa43d3f785d9443e5298149
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-26 12:04:51 -0400

    Initial changes in Home.js

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Freelancer Marketplace</h1>
+      <p>Browse projects, find freelancers, and more...</p>
+    </div>
+  );
+}
+
+export default Home;
