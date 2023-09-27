d369546a5f74e312ba71a59a5ab1566b39689097
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-27 13:27:23 -0400

    Setting up models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,69 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from config import db
 
-# Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    is_freelancer = db.Column(db.Boolean, default=False)
+    
+    projects = db.relationship('Project', backref='client', lazy=True)
+    reviews = db.relationship('Review', backref='freelancer', lazy=True)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'is_freelancer': self.is_freelancer,
+            'projects': [project.serialize() for project in self.projects],
+            'reviews': [review.serialize() for review in self.reviews]
+        }
+
+class Project(db.Model, SerializerMixin):
+    __tablename__ = 'projects'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String(500), nullable=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'image': self.image,
+            'client_id': self.client_id
+        }
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    freelancer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'content': self.content,
+            'freelancer_id': self.freelancer_id
+        }
