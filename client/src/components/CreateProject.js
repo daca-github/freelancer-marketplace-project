84010801a808ac08d1277eb6df83d2955401bcdf
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-27 17:23:42 -0400

    Initial changes on CreateProject

--- client/src/components/CreateProject.js
+++ client/src/components/CreateProject.js
@@ -1,14 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles/CreateProject.css'
 
 function CreateProject() {
+    const [projects, setProjects] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    const fetchProjects = () => {
+        axios.get('http://localhost:5555/projects')
+            .then(response => {
+                setProjects(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching projects!", error);
+            });
+    };
+
+    const handlePostProject = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:5555/projects', {
+            title: title,
+            description: description,
+            image: image,
+        })
+        .then(response => {
+            console.log(response.data);
+            fetchProjects();
+            setTitle('');
+            setDescription('');
+            setImage('');
+        })
+        .catch(error => {
+            console.error("Error posting project!", error);
+        });
+    };
+
+    const handleDeleteProject = (projectId) => {
+        axios.delete(`http://localhost:5555/projects/${projectId}`)
+            .then(response => {
+                console.log(response.data);
+                fetchProjects();
+            })
+            .catch(error => {
+                console.error("Error deleting project!", error);
+            });
+    };
+
     return (
         <div>
             <h2>Create a New Project</h2>
-            <form>
-                <input type="text" placeholder="Project Title" required />
-                <textarea placeholder="Project Description" required></textarea>
+            <form onSubmit={handlePostProject}>
+                <input 
+                    type="text" 
+                    placeholder="Project Title" 
+                    value={title} 
+                    onChange={e => setTitle(e.target.value)} 
+                    required 
+                />
+                <textarea 
+                    placeholder="Project Description" 
+                    value={description} 
+                    onChange={e => setDescription(e.target.value)} 
+                    required
+                />
+                <input 
+                    type="text" 
+                    placeholder="Image URL" 
+                    value={image} 
+                    onChange={e => setImage(e.target.value)} 
+                    required 
+                />
                 <button type="submit">Create Project</button>
             </form>
+            <h3>Your Projects</h3>
+            <ul>
+                {projects.map(project => (
+                    <li key={project.id}>
+                        {project.title} - {project.description}
+                        <button onClick={() => handleDeleteProject(project.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
