103bd1ec8f6d9ff677d96e108569463045902ea6
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-27 17:23:55 -0400

    Added CreateProject to the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faProjectDiagram, faUserGear } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faPlusSquare, faProjectDiagram, faUserGear } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav>
       <Link to="/"><li>Home <FontAwesomeIcon icon={faHome} /></li></Link>
       <Link to="/projects"><li>Projects <FontAwesomeIcon icon={faProjectDiagram} /></li></Link>
+      <Link to="/create-project"><li>Create Project <FontAwesomeIcon icon={faPlusSquare} /></li></Link>
       <Link to="/login"><li>Login <FontAwesomeIcon icon={faUserGear} /></li></Link>
       
     </nav>
