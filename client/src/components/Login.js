ee4c00326ad0e0f76ce91fd7bf6ae7ab33338d0e
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-27 12:50:14 -0400

    Added fade-in effect to login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,41 +22,45 @@ function Login() {
     });
 
     return (
-        <div>
-            <h2>{isLogin ? "Login" : "Register"}</h2>
-            <form onSubmit={formik.handleSubmit}>
-                {!isLogin && (
+        <div className="home-container fade-in">
+        <div className="container">
+            <h2>{isLogin ? "Login to Freelancer Marketplace" : "Register for Freelancer Marketplace"}</h2>
+            <div className="card">
+                <form onSubmit={formik.handleSubmit}>
+                    {!isLogin && (
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                {...formik.getFieldProps('name')}
+                            />
+                            {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+                        </div>
+                    )}
                     <div>
                         <input
-                            type="text"
-                            placeholder="Name"
-                            {...formik.getFieldProps('name')}
+                            type="email"
+                            placeholder="Email"
+                            {...formik.getFieldProps('email')}
                         />
-                        {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+                        {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
                     </div>
-                )}
-                <div>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        {...formik.getFieldProps('email')}
-                    />
-                    {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        {...formik.getFieldProps('password')}
-                    />
-                    {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
-                </div>
-                <button type="submit">{isLogin ? "Login" : "Register"}</button>
-            </form>
-            <button onClick={() => setIsLogin(!isLogin)}>
+                    <div>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            {...formik.getFieldProps('password')}
+                        />
+                        {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                    </div>
+                    <button type="submit" className="button">{isLogin ? "Login" : "Register"}</button>
+                </form>
+            </div>
+            <button onClick={() => setIsLogin(!isLogin)} className="button">
                 {isLogin ? "Need an account? Register" : "Already have an account? Login"}
             </button>
         </div>
+    </div>
     );
 }
 
