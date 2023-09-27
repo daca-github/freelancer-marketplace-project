f466373a53a394430a486d0a61ebca4ace890167
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-27 16:10:37 -0400

    Changed static data to api fetch request

--- client/src/components/ProjectListing.js
+++ client/src/components/ProjectListing.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import './styles/ProjectListing.css';
+import React, { useState, useEffect } from 'react';
+import './styles/ProjectListing.css'
 
 function ProjectListing() {
-  // Static data for now. This will be fetched from the backend later.
-  const projects = [
-    { id: 1, title: "Build a Website", imageUrl: "https://entrepreneurhandbook.co.uk/wp-content/uploads/2019/10/Website-builder.jpg.webp" },
-    { id: 2, title: "Design a Logo", imageUrl: "https://res.cloudinary.com/zenbusiness/image/upload/v1670445040/logaster/logaster-2019-04-2.-a-by-nick-matey.jpg" }
-  ];
-  
+    const [projects, setProjects] = useState([]);
 
-  return (
-    <div className="home-container fade-in">
-    <div className="project-listing-container">
-      <h2>Available Projects</h2>
-      <div className="projects-grid">
-        {projects.map(project => (
-          <div key={project.id} className="project-card">
-            <img src={project.imageUrl} alt={project.title} className="project-image" />
-            <a href={`/projects/${project.id}`}>{project.title}</a>
-          </div>
-        ))}
-      </div>
-    </div>
-  </div>
-  );
-  
+    useEffect(() => {
+        // Fetch projects from the backend
+        fetch('http://localhost:5555/projects')
+            .then(response => response.json())
+            .then(data => setProjects(data))
+            .catch(error => console.error('Error fetching projects:', error));
+    }, []);
+
+    return (
+        <div className="project-listing-container">
+            <h2>Available Projects</h2>
+            <div className="projects-grid">
+                {projects.map(project => (
+                    <div key={project.id} className="project-card">
+                        <a href={`/projects/${project.id}`}>
+                            <div className="project-image-container">
+                                <img className="project-image" src={project.image} alt={project.title} />
+                            </div>
+                            <h3>{project.title}</h3>
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default ProjectListing;
