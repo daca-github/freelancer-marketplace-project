dac353c5387edc469779c9d34a0b3d6dee3af8b8
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-27 16:12:43 -0400

    Added route to display projects details by id

--- server/app.py
+++ server/app.py
@@ -5,8 +5,7 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 
 from config import app, db
-from models import User, Project, Review, db
-
+from models import User, Project, Review
 
 api = Api(app)
 CORS(app)
@@ -20,23 +19,12 @@ def get_projects():
     projects = Project.query.all()
     return jsonify([project.serialize() for project in projects])
 
-
-class ProjectListResource(Resource):
-    def get(self):
-        projects = Project.query.all()
-        return jsonify([project.to_dict() for project in projects])
-
-api.add_resource(ProjectListResource, '/api/projects')
-
-class ProjectResource(Resource):
-    def get(self, project_id):
-        project = Project.query.get_or_404(project_id)
-        return project.to_dict()
-
-api.add_resource(ProjectResource, '/api/projects/<int:project_id>')
+@app.route('/projects/<int:project_id>', methods=['GET'])
+def get_project_details(project_id):
+    project = Project.query.get_or_404(project_id)
+    return jsonify(project.serialize())
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(port=5555, debug=True)
-
